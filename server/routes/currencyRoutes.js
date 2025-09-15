abc36a88a3dab300861b1c497483a759ba337b47
omranmahmoud <omranmahmoud888@gmail.com>
2025-09-15 16:44:07 +0300

    Delete server directory

--- server/routes/currencyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { convertCurrency } from '../controllers/currencyController.js';
-
-const router = express.Router();
-
-router.post('/convert', convertCurrency);
-
-export default router;
