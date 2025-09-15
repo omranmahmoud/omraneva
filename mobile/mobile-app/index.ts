b7c5d0cb8bdb30b23cd75f603cd6ea1560e35ca7
omranmahmoud <omranmahmoud888@gmail.com>
2025-09-15 16:46:30 +0300

    Delete mobile directory

--- mobile/mobile-app/index.ts
+++
@@ -1,3 +0,0 @@
-// This TypeScript entry defers to index.js to avoid duplicate registrations.
-// Keep for IDE/TS support only.
-import './index.js';
--- mobile/mobile-app/src/services/settingsService.ts
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export async function fetchStoreSettings() {
-  const res = await api.get('/api/settings');
-  return res.data;
-}
